1a8732957256132a7df6297d5a063d079c2a6c62
guanxiaochao <guanxiaochao@gmail.com>
2014-06-02 18:39:38 +0800

    commit8

--- test/test.js
+++ test/test.js
@@ -15,3 +15,24 @@ describe('Test Framework', function() {
     })
 
 })
+
+describe ('Asynchronous testing', function() {
+
+    var result = false;
+
+    beforeEach (function (done) {
+
+        setTimeout (function () {
+             result = true;
+             //complete the async beforeEach
+             done ();
+        }, 2000);
+    });
+
+
+
+it ('should NOT be so hard so this makes it easier.', function (done) {
+    result.should.eql(true);
+    done();
+   })
+})
